d8e3b6463e44e594bea79d9f70681629ecafb454
Nataliia_Lushnytska <natalochka2509@gmail.com>
2026-05-31 19:51:15 +0300

    Add files via upload

---
+++ src/pages/__init__.py
@@ -0,0 +1,4 @@
+from .base_page import BasePage
+from .events_page import EventsPage
+
+__all__ = ['BasePage', 'EventsPage']
---
+++ src/pages/events_page.py
@@ -0,0 +1,37 @@
+from selenium.webdriver.common.by import By
+from .base_page import BasePage
+
+class EventsPage(BasePage):
+    """Events Page Object"""
+    
+    # Locators
+    EVENTS_CONTAINER = (By.CSS_SELECTOR, "[class*='events-container']")
+    EVENT_CARDS = (By.CSS_SELECTOR, "[class*='event-card']")
+    FILTER_BUTTON = (By.CSS_SELECTOR, "[class*='filter-btn']")
+    EVENT_TITLE = (By.CSS_SELECTOR, "[class*='event-title']")
+    EVENT_DATE = (By.CSS_SELECTOR, "[class*='event-date']")
+    
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.page_url = f"{self.base_url}/events"
+    
+    def navigate_to_events(self):
+        """Navigate to events page"""
+        self.navigate_to(self.page_url)
+    
+    def get_events_count(self) -> int:
+        """Get count of event cards"""
+        return len(self.find_elements(self.EVENT_CARDS))
+    
+    def get_event_titles(self) -> list:
+        """Get all event titles"""
+        elements = self.find_elements(self.EVENT_TITLE)
+        return [elem.text for elem in elements]
+    
+    def click_filter_button(self):
+        """Click filter button"""
+        self.click_element(self.FILTER_BUTTON)
+    
+    def is_events_loaded(self) -> bool:
+        """Check if events are loaded"""
+        return self.is_element_visible(self.EVENTS_CONTAINER)
